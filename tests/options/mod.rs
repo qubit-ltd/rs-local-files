6b1a0138a3f095ac59cb61cbed511793c5e5caaa
Haixing Hu <starfish.hu@gmail.com>
2026-08-02 02:48:45 +0800

    chore: complete source license headers

--- tests/options/mod.rs
+++ tests/options/mod.rs
@@ -2,6 +2,8 @@
 //    Copyright (c) 2026 Haixing Hu.
 //
 //    SPDX-License-Identifier: Apache-2.0
+//
+//    Licensed under the Apache License, Version 2.0.
 // =============================================================================
 
 mod local_copy_source_mode_tests;
